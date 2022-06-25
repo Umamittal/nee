b554323ee97b5969a33783fc51bc008800ca7c7d
Umamittal <108165211+Umamittal@users.noreply.github.com>
2022-06-25 08:47:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,145 @@
+
+
+
+function App() {
+  return (
+
+<div className="relative py-14 drop-shadow-2xl bg-gradient-to-b from-slate-400 to-purple-500">
+<img className="w-4/5 rounded-r-full h-80 xl:w-96" alt="" src="https://thumbs.dreamstime.com/b/silver-pot-full-golden-coins-black-background-leprechaun-treasure-concept-golden-coins-silver-pot-black-background-195554385.jpg"/>
+<div className="mx-3 my-4 text-4xl font-bold text-white"> The Most Trusted & Secure Crypto Wallet </div>
+<div className="mx-3 text-white"> Best trading platform and more reliable financial transactions.</div>
+
+
+<button class="inline-flex  items-center mx-36 xl:mx-96 text-white h-14  border-0 py-1 px-3  focus:outline-none hover:bg-gray-200 hover:text-black rounded-full text-base mt-4 md:mt-0 xl:mt-6">
+<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class=" h-10 mx-2  text-white bg-indigo-700 rounded-full  shadow-xl shadow-blue-400 " viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+  Swipe to get started 
+      
+    </button>
+
+
+
+
+
+
+</div>
+
+//     <div className="mx-4 my-2 shadow-2xl drop-shadow-2xl">
+//      <div className="mx-4 text-3xl font-bold bg-gray-200 "> Summer Art Camp! 5 Days of Artists 
+//      and Painting Monet, Van Gogh, Matisse, & More   </div>
+//      <div className="mx-3 font-medium text-slate-600"> Multi-Day Course  <span className="px-2 text-gray-600 rounded-full bg-slate-300"> 
+//      ? </span>
+//     </div>
+//     <div className="container flex mx-4 my-4">
+//     <div className="">
+//   <div className="container flex flex-col items-center px-5 py-20 mx-auto md:flex-row">
+//     <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+//       <p1 className="mb-10 text-xl font-normal "> In this 5 day class we will explore artists Monet, Matisse, Van Gogh, among others and then recreate paintings using crayon and watercolor.
+//        Students will have fun learning about the artists & creating their own their own art inspired by their work.
+//        <p className="container flex mt-10"> 
+//        <img className="rounded-full w-7" alt="" 
+//        src="https://cdn4.vectorstock.com/i/1000x1000/64/68/beautiful-girl-logo-design-template-spa-or-vector-3936468.jpg"    />
+//          <span className="mx-3 text-2xl font-bold text-purple-700"> Kimberly R Moseler </span> 
+//        </p>     
+
+//        <section class="text-gray-600  overflow-hidden">
+//   <div class="container -mx-24 mt-3 ">
+//     <div class="lg:w-4/5 mx-auto flex flex-wrap">
+     
+//       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-5 lg:mt-0">
+     
+//         <div class="flex ">
+//           <span class="flex ">
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentcolor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <span class="text-gray-600 mx-3 flex -my-0 text-xs ">467 total reviews </span>
+//           </span>
+        
+//         </div>
+       
+       
+//        </div>
+       
+//     </div>
+//   </div>
+// </section>  
+// <section class="text-gray-600  overflow-hidden">
+//   <div class="container -mx-24 -my-6 ">
+//     <div class="lg:w-4/5 mx-auto flex flex-wrap">
+     
+//       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6  lg:mt-0">
+     
+//         <div class="flex mb-4">
+//           <span class="flex ">
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <svg fill="currentcolor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+//               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+//             </svg>
+//             <span class="text-gray-600 mx-3 flex -my-0 text-xs ">5 reviews </span>
+//           </span>
+        
+//         </div>
+       
+       
+//        </div>
+       
+//     </div>
+//   </div>
+// </section>
+// <div className="mb-10 font-medium"> Completed by 21 learners </div> 
+// <button class="flex-shrink-0 text-white bg-purple-700 border-0 py-2 mt-12  px-8 focus:outline-none  hover:bg-purple-600 rounded-full text-lg  sm:mt-0 font-medium"> See class Schedule > </button>
+// <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+//             <svg fill="none"  stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-36  text-purple-700 h-5" viewBox="0 0 24 24">
+//               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+//             </svg>
+//             </button>
+//             <span className="-mx-3 font-bold text-purple-900 "> Save          </span>
+//             <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4 ">
+//             <svg fill="none"  stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-36  text-purple-700 h-5 ml-5 " viewBox="0 0 24 24">
+//   <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+// </svg> 
+// </button>
+
+//       </p1>
+//       <span className="-my-20 text-xl font-bold text-purple-900 mx-96 twfont-bold"> Share       </span>
+//    </div>
+//     <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
+//       <img className="object-cover object-center rounded" alt="hero"
+//        src="https://st3.depositphotos.com/1025323/12863/i/950/depositphotos_128631902-stock-photo-paint-explosion-background.jpg" />
+//     </div>
+    
+//   </div>
+// </div>
+
+//     </div>
+//     </div>
+    
+  );
+}
+
+export default App;
